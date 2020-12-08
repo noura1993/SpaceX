5c0d5694e7baa51b9ae6b8b7735ab534ba247306
Nourhan Elhakim <nourhanmagdy993@gmail.com>
2020-12-08 20:56:50 +0000

    Implement Logo component and move assets

---
+++ src/components/Logo.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Logo.css";
+import spacex from "../assets/spacex-logo.png";
+
+const Logo = () => {
+  return (
+    <div className="logo-with-title">
+        <img className="logo" src={spacex} />
+        <span className="logo-title">LAUNCHES</span>
+    </div>
+  );
+};
+
+export default Logo;
---
+++ src/containers/HomePageContainer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Logo from '../components/Logo';
+
+const HomePageContainer = () => {
+
+    return (
+        <div className='HomePage'>
+            <Logo />
+        </div>
+    );
+}
+
+export default HomePageContainer;
