53976509bead1b2ceceff8290ad5ed373739d6ec
Nourhan Elhakim <nourhanmagdy993@gmail.com>
2020-12-11 01:29:54 +0000

    Fix some warnings in filter

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -9,11 +9,11 @@ const Filter = ({years, filterByYear}) => {
 
   return (
     <div className="filter-wrapper">
-      <select name="years" id="years" onChange={(e) => onChange(e.target.value)}>
-        <option selected disabled>Filter By Year</option>
+      <select defaultValue="Filter By Year" name="years" id="years" onChange={(e) => onChange(e.target.value)}>
+        <option disabled>Filter By Year</option>
         <option value='Show all'>Show all</option>
         {years.map( year => {
-          return <option value={year}>{year}</option>
+          return <option key={year} value={year}>{year}</option>
         })}
       </select>
     </div>
