6f6cb7ed3e57d6abba827ceb5dbb92e2a4d426c4
Nourhan Elhakim <nourhanmagdy993@gmail.com>
2020-12-12 16:09:50 +0000

    Add test for img component

---
+++ src/__tests__/components/image/Image.test.js
@@ -0,0 +1,9 @@
+import { render } from "@testing-library/react";
+import Image from "../../../components/Image";
+
+test("img with alt", () => {
+  const { container } = render(<Image />);
+  const img = container.querySelector('img');
+  expect(img.src).toContain('launch-home.png');
+  expect(img.alt).toEqual('Rocket launch');
+});
