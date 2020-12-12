2fcea39eb0f3d5b8dbff404ce082238cf80715b2
Nourhan Elhakim <nourhanmagdy993@gmail.com>
2020-12-12 16:43:37 +0000

    Add test for reload click

--- src/__tests__/components/buttons/Reload.test.js
+++ src/__tests__/components/buttons/Reload.test.js
@@ -1,12 +1,24 @@
 import { render } from "@testing-library/react";
 import Reload from "../../../components/Reload";
+import { Simulate } from 'react-dom/test-utils';
 
 test("reload has a button with img", () => {
   const { container } = render(<Reload />);
-  const button = container.querySelector('button');
-  const img = button.querySelector('img');
+  const button = container.querySelector("button");
+  const img = button.querySelector("img");
 
-  expect(img.src).toContain('refresh@3x.png');
-  expect(img.alt).toEqual('reload icon');
-  expect(button.innerHTML).toContain('Reload Data');
+  expect(img.src).toContain("refresh@3x.png");
+  expect(img.alt).toEqual("reload icon");
+  expect(button.innerHTML).toContain("Reload Data");
+});
+
+test("reload has onClick", () => {
+  let counter = 0;
+  const testFunction = () => {
+      counter++;
+  }
+  const { container } = render(<Reload reload={testFunction}/>);
+  const button = container.querySelector("button");
+  Simulate.click(button);
+  expect(counter).toEqual(1);
 });
