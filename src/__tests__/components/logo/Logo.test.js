fd3e6ae6cde226410d068e9278206801acaf9720
Nourhan Elhakim <nourhanmagdy993@gmail.com>
2020-12-12 16:19:08 +0000

    Add test for logo component

---
+++ src/__tests__/components/logo/Logo.test.js
@@ -0,0 +1,12 @@
+import { render } from "@testing-library/react";
+import Logo from "../../../components/Logo";
+
+test("logo has img & title", () => {
+  const { container } = render(<Logo />);
+  const img = container.querySelector('img');
+  const logoText = container.querySelector('span');
+
+  expect(img.src).toContain('spacex-logo.png');
+  expect(img.alt).toEqual('spacex logo');
+  expect(logoText.innerHTML).toEqual('LAUNCHES');
+});
